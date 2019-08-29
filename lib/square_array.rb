8845af4dd8083a06c44d16b587301204e3e6841e
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-29 22:12:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
-def square_array(array)
-  # your code here
+numbers = [1, 2, 3] 
+
+def square_array(numbers)
+  counter = 0
+  
+  while numbers[counter] do
+    (numbers[counter]) ** 2 => new_numbers
+    counter += 1
+  end
+  
+  
 end
---
+++ test.rb
@@ -0,0 +1,45 @@
+numbers = [1, 2, 3] 
+
+def square_array(numbers)
+  counter = 0
+  
+  while numbers[counter] do
+    p (numbers[counter]) ** 2
+    counter += 1
+  end
+end
+
+square_array(numbers)
+
+
+
+def square_array(array)
+  numbers = [1, 2, 3]
+square_array(numbers)
+new_numbers = [square_array(numbers)]
+while square_array(numbers) do 
+  puts new_numbers
+end
+end
+
+def square_array(array)
+  counter = 0
+ 
+  while array[counter] do
+    puts ((array[counter]) ** 2) => new_array  
+    counter += 1
+  end
+end
+
+puts (numbers[counter]) ** 2 
+
+numbers = [1, 2, 3]
+
+def square_array(numbers)
+  counter = 0
+  
+  while numbers[counter] do
+    p (numbers[counter]) ** 2
+    counter += 1
+  end
+end
